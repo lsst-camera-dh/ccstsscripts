1bc273aaa3efaca23efc2ce44f137f70825be514
Homer Neal <homer@slac.stanford.edu>
2014-11-05 14:52:40 -0500

    with BNL mods

---
+++ TS3_JH_acq/jobs/T03_flat_BNL/v0/producer_T03_flat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from PythonBinding import *
+import os
+#import lsst.eotest.sensor as sensorTest
+import sys
+#from lcatr.harness.helpers import dependency_glob
+
+fileName = "/home/homer/lsst/lcatr/share/T03_flat/v0/ccsacqflat.py"
+fo = open(fileName, "r");
+content = fo.read();
+fo.close();
+
+try:
+#Create an instance of the python binding
+    ccs1 = CcsJythonInterpreter();
+ 
+ 
+    print 'starting synch execution'
+    result1 = ccs1.syncExecution(content);
+    print result1.getOutput();    
+ 
+ 
+except CcsException as ex:
+    print 'Failure', ex
+
