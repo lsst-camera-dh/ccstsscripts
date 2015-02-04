06995b9f44b41b09d43e8504e9fa40fd55cffa44
Homer A. Neal Jr <homer@rhel6-64d.slac.stanford.edu>
2015-02-04 10:53:04 -0800

    new version of flat acq scripts

---
+++ TS3_JH_acq/jobs/T03_flat/v2/validator_T03_flat.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import glob
+#import numpy as np
+import lcatr.schema
+import hdrtools as ht
+    
+results = []
+
+cdir = "/home/homer/lsst/lcatr/share/T03_flat/v0";
+
+fo = open("%s/status.out" % (cdir), "r");
+tsstat = fo.readline();
+tsvolt = fo.readline();
+tscurr = fo.readline();
+tspres = fo.readline();
+tstemp = fo.readline();
+fo.close();
+
+results.append(lcatr.schema.valid(lcatr.schema.get('T03_flat'),stat=tsstat,volt=tsvolt,curr=tscurr,pres=tspres,temp=tstemp))
+
+files = glob.glob('%s/ArchonImage*.fits' % (cdir))
+bias = ht.fitsAverage('%s/ArchonImage_Bias.fits' % (cdir));
+    
+for fname in files
+    avg = ht.fitsAverage(fname)
+    print "DATA : Average signal = %8.2f DN" % (avg - bias)
+    
+data_products = [lcatr.schema.fileref.make(item) for item in files]
+results.extend(data_products)
+
+lcatr.schema.write_file(results)
+lcatr.schema.validate_file()
+        
